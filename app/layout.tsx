a9d503cdc593f3468bd528a007552256281e5d1e
arbone <shehuarbi1@gmail.com>
2025-06-23 09:52:31 +0200

    Il tuo messaggio

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body className={`${inter.variable} font-body antialiased`}>
         <SessionProvider> {/* SessionProvider MUST wrap the core content for NextAuth client features */}
